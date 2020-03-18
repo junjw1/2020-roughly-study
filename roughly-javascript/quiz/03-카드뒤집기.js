00d3dd4c0bd09e823e9c09df3065314a84766a4b
timetoplay <junjw1@daum.net>
2020-03-18 23:37:23 +0900

    'quiz3'

---
+++ roughly-javascript/quiz/03-카드뒤집기.js
@@ -0,0 +1,27 @@
+
+var cardNum = 100;
+var arr = [];
+
+
+//뒷면 0, 앞면 -1
+for(var i=1 ; i<=cardNum ; i++){
+    arr.push(0);
+}
+
+for(var i=2 ; i<=cardNum ; i++){
+    
+    console.log(i + '번째 카드부터 ' + (i-1) + '장 간격으로 뒤집기');
+
+    for(var k=i ; k<=cardNum ; k=k+i){
+        arr[k-1] = ~(arr[k-1]);
+    }
+
+    console.log(arr);
+}
+
+//값이 0인 인덱스 출력
+for(var i=0 ; i<cardNum ; i++){
+    if(arr[i] == 0){
+        console.log(i+1);
+    }
+}
